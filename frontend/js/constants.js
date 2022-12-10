7dd54c0f7cdc98d2d16a91ee71aba93d6b6578fd
ogunfaith <dafpizzle@gmail.com>
2022-12-10 13:52:13 +0100

    goerli test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x8624A89F0281b7E4E0B14AAC817727beA3ee11d5"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
