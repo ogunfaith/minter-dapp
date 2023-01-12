676cf6ebd19ad1957cd8be77cf6f8b13c7504ce2
ogunfaith <dafpizzle@gmail.com>
2023-01-12 14:54:56 +0100

    updates

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
-const contractAddress = "0x68F6EA51c92D22406549abFcecFc3BC58eDE4C93"; // Replace with your own contract address
+const contractAddress = "0xe3Ee31c95a579D86B5937aFAA752555cBc8A1Fd0"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the FAFI NFT Project!!";
+const welcome_h1 = "Welcome to the FAFI NFT Project!!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The FAFI22 NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
